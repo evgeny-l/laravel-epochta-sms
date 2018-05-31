67ab8874a36d03b497efea9f6ba91e8fc1ac55ea
evgenyl <lexunin@gmail.com>
2018-05-31 22:07:06 +0300

    Initial commit.

---
+++ src/LaravelEPochtaRuSMSServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace EvgenyL\LaravelEPochtaRuSMS;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelEPochtaRuSMSServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+
+}
